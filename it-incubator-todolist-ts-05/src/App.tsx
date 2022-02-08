492470998fd58796e3f9b8d6ee1039b3e222c940
Irina Novakovich <irinaredhood@gmail.com>
2022-02-08 18:00:58 +0300

    changed structure of todolist using map

--- it-incubator-todolist-ts-05/src/App.tsx
+++ it-incubator-todolist-ts-05/src/App.tsx
@@ -5,6 +5,12 @@ import {v1} from 'uuid';
 
 export type FilterValuesType = "all" | "active" | "completed";
 
+export type TodolistsType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+
 function App() {
 
     let [tasks, setTasks] = useState([
@@ -14,7 +20,13 @@ function App() {
         {id: v1(), title: "Rest API", isDone: false},
         {id: v1(), title: "GraphQL", isDone: false},
     ]);
-    let [filter, setFilter] = useState<FilterValuesType>("all");
+
+    let [todolists, setTodolists] = useState<Array<TodolistsType>>([
+        {id: v1(), title: "What to learn", filter: 'all'},
+        {id: v1(), title: "What to buy", filter: 'all'}
+    ])
+
+    //let [filter, setFilter] = useState<FilterValuesType>("all");
 
 
     function removeTask(id: string) {
@@ -40,28 +52,49 @@ function App() {
 
     let tasksForTodolist = tasks;
 
-    if (filter === "active") {
+    /*if (filter === "active") {
         tasksForTodolist = tasks.filter(t => t.isDone === false);
     }
     if (filter === "completed") {
         tasksForTodolist = tasks.filter(t => t.isDone === true);
-    }
+    }*/
 
     function changeFilter(value: FilterValuesType) {
-        setFilter(value);
+        //setFilter(value);
     }
 
 
     return (
         <div className="App">
-            <Todolist title="What to learn"
+            {todolists.map((tl, index) => {
+
+                if (tl.filter === "active") {
+                    tasksForTodolist = tasks.filter(t => t.isDone === false);
+                }
+                if (tl.filter === "completed") {
+                    tasksForTodolist = tasks.filter(t => t.isDone === true);
+                }
+
+                return (
+                    <Todolist title={tl.title}
+                              tasks={tasksForTodolist}
+                              removeTask={removeTask}
+                              changeFilter={changeFilter}
+                              addTask={addTask}
+                              changeTaskStatus={changeStatus}
+                              filter={tl.filter}
+                    />
+                )
+            })}
+
+            {/*<Todolist title="What to learn"
                       tasks={tasksForTodolist}
                       removeTask={removeTask}
                       changeFilter={changeFilter}
                       addTask={addTask}
                       changeTaskStatus={changeStatus}
                       filter={filter}
-            />
+            />*/}
         </div>
     );
 }
