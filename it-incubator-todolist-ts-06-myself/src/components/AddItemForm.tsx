4f76eda951a4fd02b64708e1178c1e4f46ad66dc
Irina Novakovich <irinaredhood@gmail.com>
2022-04-29 10:49:35 +0300

    transfered callback function from ToDoList to AddItemForm

--- it-incubator-todolist-ts-06-myself/src/components/AddItemForm.tsx
+++ it-incubator-todolist-ts-06-myself/src/components/AddItemForm.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 
-const AddItemForm = () => {
+export type AddItemFormPropsType = {
+    callback: () => void
+}
+
+export const AddItemForm = () => {
+
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addTask = () => {
+        let newTitle = title.trim();
+        if (newTitle !== "") {
+            props.addTask(newTitle);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value)
+}
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addTask();
+        }
+    }
+
     return (
         <div>
-            
+            <input value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   className={error ? "error" : ""}
+            />
+            <button onClick={addTask}>+</button>
+            {error && <div className="error-message">{error}</div>}
         </div>
     );
 };
-
-export default AddItemForm;
