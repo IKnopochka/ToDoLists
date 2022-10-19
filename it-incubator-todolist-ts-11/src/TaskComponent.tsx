3e80308c56db22980ba850b538f648747d17b55e
Irina Novakovich <irinaredhood@gmail.com>
2022-10-19 14:11:53 +0300

    fixed mistakes using useCallback and React.memo

---
+++ it-incubator-todolist-ts-11/src/TaskComponent.tsx
@@ -0,0 +1,41 @@
+import React, {ChangeEvent, memo, useCallback} from 'react';
+import {Checkbox, IconButton} from "@material-ui/core";
+import {EditableSpan} from "./EditableSpan";
+import {Delete} from "@material-ui/icons";
+import {TaskType} from "./Todolist";
+
+type TaskComponentType = {
+    removeTask: (taskId: string) => void
+    changeTaskStatus: (id: string, isDone: boolean) => void
+    changeTaskTitle: (taskId: string, newTitle: string) => void
+    task: TaskType
+}
+
+const TaskComponent = memo(({changeTaskTitle, ...props}: TaskComponentType) => {
+    console.log('Task')
+    const onClickHandler = () => props.removeTask(props.task.id)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        let newIsDoneValue = e.currentTarget.checked;
+        props.changeTaskStatus(props.task.id, newIsDoneValue);
+    }
+    const onTitleChangeHandler = useCallback((newValue: string) => {
+        changeTaskTitle(props.task.id, newValue);
+    }, [changeTaskTitle, props.task.id])
+
+
+    return <div className={props.task.isDone ? "is-done" : ""}>
+        <Checkbox
+            checked={props.task.isDone}
+            color="primary"
+            onChange={onChangeHandler}
+        />
+
+        <EditableSpan value={props.task.title} onChange={onTitleChangeHandler}/>
+        <IconButton onClick={onClickHandler}>
+            <Delete/>
+        </IconButton>
+    </div>
+
+})
+
+export default TaskComponent;
