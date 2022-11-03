4ca2d3ccc85039604017f753943c0db31260a35d
Irina Novakovich <irinaredhood@gmail.com>
2022-11-03 19:26:25 +0300

    task 2 optimized

--- task_2/src/examples/example_3/Example_3.tsx
+++ task_2/src/examples/example_3/Example_3.tsx
@@ -1,4 +1,4 @@
-import {FC, memo, useState} from 'react';
+import {FC, memo, useCallback, useMemo, useState} from 'react';
 
 const FIRST_BUTTON_BACKGROUND = {background: 'tomato'};
 const CONTAINER_STYLES = {display: 'flex', flexDirection: 'column', gap: 10};
@@ -14,16 +14,20 @@ export const Example_3 = () => {
 
     const [firstCount, setFirstCount] = useState(0);
 
-    const handlePlusCountValueClick = () => setFirstCount(prevFirstCount => prevFirstCount + 1);
+    const handlePlusCountValueClick = useCallback(() => setFirstCount(prevFirstCount => prevFirstCount + 1), [firstCount]);
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleSetIsChecked = useCallback(() => setIsChecked(!isChecked), [isChecked]);
 
     return (
         <div style={{...CONTAINER_STYLES} as any}>
             <div>{`Count equal: ${firstCount}`}</div>
             <Button onPlusCountValueClick={handlePlusCountValueClick}/>
-            <Checkbox/>
+            <Checkbox isChecked={isChecked} onSetIsChecked={handleSetIsChecked}/>
         </div>
     );
-};
+}
 
 export const Button: FC<{ onPlusCountValueClick: () => void }> = memo(({onPlusCountValueClick}) => {
     console.log('Button render')
@@ -39,13 +43,11 @@ export const Button: FC<{ onPlusCountValueClick: () => void }> = memo(({onPlusCo
     );
 });
 
-export const Checkbox= memo(() => {
+export const Checkbox= memo(({isChecked, onSetIsChecked}: PropsType) => {
     console.log('Checkbox render')
 
-    const [isChecked, setIsChecked] = useState(false);
 
-    const handleSetIsChecked = () => setIsChecked(!isChecked);
     return (
-        <input type="checkbox" checked={isChecked} onChange={handleSetIsChecked}/>
+        <input type="checkbox" checked={isChecked} onChange={onSetIsChecked}/>
     );
 });
--- task_3/src/examples/slowComponent/SlowComponent.tsx
+++ task_3/src/examples/slowComponent/SlowComponent.tsx
@@ -1,15 +1,19 @@
-import React, {useEffect} from "react";
+import React, {useCallback, useEffect} from "react";
 
 export const SlowComponent = () => {
     console.log('SlowComponent re-render...');
 
-    let now = performance.now();
+        let now = performance.now();
 
 
         while(performance.now() - now < 1000)
-    {
-        // Artificial delay -- do nothing for 100ms
-    }
+        {
+            // Artificial delay -- do nothing for 100ms
+        }
+
+
 
     return <p>I am a very slow component tree.</p>;
 };
+
+
