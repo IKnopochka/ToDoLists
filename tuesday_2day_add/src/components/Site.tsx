df542204da678f8f568969a5c38a10bb24b415c7
Irina Novakovich <irinaredhood@gmail.com>
2023-02-06 14:49:00 +0300

    2Tuesday dops routing additional

--- tuesday_2day_add/src/components/Site.tsx
+++ tuesday_2day_add/src/components/Site.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import styles from './Site.module.css'
 import {Routes, Route, Navigate, NavLink} from 'react-router-dom'
-import {PageOne} from "./pages/PageOne";
-import {PageTwo} from "./pages/PageTwo";
-import {PageThree} from "./pages/PageThree";
 import {Error404} from "./pages/Error404";
 import {Page} from "./pages/Page";
 import {dataState} from "../dataState/dataState";
@@ -15,9 +12,20 @@ export const Site = () => {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <div><NavLink to={'/page/0'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page 1</NavLink></div>
-                    <div><NavLink to={'/page/1'} className={isActive => isActive.isActive ? styles.active : styles.navLink}>Page 2</NavLink></div>
-                    <div><NavLink to={'/page/2'} className={isActive => isActive.isActive ? styles.active : styles.navLink}>Page 3</NavLink></div>
+                    <div>
+                        <NavLink to={'/page/0'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page
+                            1</NavLink>
+                    </div>
+                    <div>
+                        <NavLink to={'/page/1'}
+                                 className={isActive => isActive.isActive ? styles.active : styles.navLink}>Page
+                            2</NavLink>
+                    </div>
+                    <div>
+                        <NavLink to={'/page/2'}
+                                 className={isActive => isActive.isActive ? styles.active : styles.navLink}>Page
+                            3</NavLink>
+                    </div>
                     {/*<div><NavLink to={'/page3'} className={isActive => activeLink(isActive.isActive)}>Page 3</NavLink></div>*/}
                 </div>
                 <div className={styles.content}>
@@ -25,10 +33,12 @@ export const Site = () => {
                         <Route path={'/'} element={<Navigate to={'/page/0'}/>}/>
 
                         <Route path={'/page/:id'} element={<Page pages={dataState.pages}/>}/>
-{/*                        <Route path={'/page2'} element={<PageTwo/>}/>
-                        <Route path={'/page3'} element={<PageThree/>}/>*/}
 
+
+                        {/*                        <Route path={'/page2'} element={<PageTwo/>}/>
+                        <Route path={'/page3'} element={<PageThree/>}/>*/}
                         <Route path={'/*'} element={<Error404/>}/>
+
                     </Routes>
                 </div>
             </div>
--- tuesday_2day_add/src/components/pages/Page.tsx
+++ tuesday_2day_add/src/components/pages/Page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {DataStateType} from "../../dataState/dataState";
 import {useParams} from "react-router-dom";
+import {Error404} from "./Error404";
 
 export const Page = (props: DataStateType) => {
     const param = useParams()
-    console.log(Number(param.id))
+
+    if (Number(param.id) > 2) return <Error404/>
+
     return (
         <div>
+
             <div>{props.pages[Number(param.id)].heading}</div>
             <div>{props.pages[Number(param.id)].about}</div>
         </div>
