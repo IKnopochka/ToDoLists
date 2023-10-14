21d11c6ca6fa14ed0e59e4bd54e64a16125926c7
Irina Novakovich <irinaredhood@gmail.com>
2023-10-13 19:07:57 -0700

    fixing deploy mistake

--- it-incubator-todolist-ts-19/src/features/TodolistsList/Todolist/Todolist.tsx
+++ it-incubator-todolist-ts-19/src/features/TodolistsList/Todolist/Todolist.tsx
@@ -23,6 +23,7 @@ type PropsType = {
 }
 
 export const Todolist = React.memo(function ({demo = false, ...props}: PropsType) {
+    console.log('Todolist called')
 
     const dispatch = useDispatch()
     useEffect(() => {
